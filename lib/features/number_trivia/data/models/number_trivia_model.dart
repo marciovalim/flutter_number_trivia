24207897b391627973986e94673111c9d988c6a2
marciovalim <marciogsvalim@gmail.com>
2021-01-21 15:32:32 -0300

    added number_trivia_model

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+
+import 'package:flutter_number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  NumberTriviaModel({
+    @required String text,
+    @required int number,
+  }) : super(number: number, text: text);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'text': text,
+      'number': number,
+    };
+  }
+
+  factory NumberTriviaModel.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    return NumberTriviaModel(
+      text: map['text'],
+      number: map['number'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory NumberTriviaModel.fromJson(String source) =>
+      NumberTriviaModel.fromMap(json.decode(source));
+}
---
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_number_trivia/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:flutter_number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final tText = "this is a text of test";
+  final tNumber = 1;
+  final tResultFromApi = json.encode({
+    "text": tText,
+    "number": tNumber,
+  });
+
+  group('number_trivia_model', () {
+    test('number_trivia_model.fromJson should return an instance of itself',
+        () {
+      final numberTriviaModel = NumberTriviaModel.fromJson(tResultFromApi);
+
+      final expected = NumberTriviaModel(number: tNumber, text: tText);
+      expect(numberTriviaModel, expected);
+    });
+
+    test('number_triva_model.toJson should return a json encoded of itself',
+        () {
+      final numberTriviaModel = NumberTriviaModel.fromJson(tResultFromApi);
+
+      final result = numberTriviaModel.toJson();
+
+      expect(result, tResultFromApi);
+    });
+
+    test('number_trivia_model is subclass of number_trivia entity', () {
+      final numberTriviaModel = NumberTriviaModel.fromJson(tResultFromApi);
+
+      expect(numberTriviaModel, isA<NumberTrivia>());
+    });
+  });
+}
